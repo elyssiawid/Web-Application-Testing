7c1eeecbf3f62242ca564901d8b8822e631c6152
Elyssia Widjaja <Elyssia@Elyssias-MBP.lan1>
2019-08-08 14:59:35 -0700

    added app.test.js

--- web-test/src/App.js
+++ web-test/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+​
+class App extends Component {
+  state = {
+    strikes: 0,
+    balls: 0,
+    hits: 0,
+    fouls: 0
+  };
+​
+  addStrike = () => {
+    this.setState({
+      strike: this.state.strike + 1
+    });
+  };
+​
+  addBall = () => {
+    this.setState({
+      ball: this.state.ball + 1
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1> Baseball</h1>
+        <p>Strikes</p>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.addStrike}>Strike</button>
+        <p>Balls</p>
+        <p>Count: {this.state.balls}</p>
+        <button>Ball</button>
+        <h1>app</h1>
+      </div>
+    );
+  }
 }
-
+​
 export default App;
